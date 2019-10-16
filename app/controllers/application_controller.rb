d9576967fc3f0f1587ee66760277b8a396b8f34e
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-10-16 07:32:51 -0600

    fix bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_frogery with: :exception
-
+  protect_from_forgery with: :exception
+  
   def hello
     render html: 'hello world from sample'
   end
