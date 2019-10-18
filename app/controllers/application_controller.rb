d3b0e3838f0bb11cba2d0c109bad80883798bea4
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-10-18 10:38:28 -0600

    add contact

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: 'hello world from sample'
-  end
+  # def hello
+  #   render html: 'hello world from sample'
+  # end
 end
