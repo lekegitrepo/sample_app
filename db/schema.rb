1038506491bcac95483dceabd827bced29658a50
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-10-27 17:33:03 -0600

    add test cases

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2019_10_27_200754) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_users_on_email"
   end
 
 end
