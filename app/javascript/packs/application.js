8517b05b29009e7ff9dca674366d734cb8417f8f
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-10-30 15:33:13 -0600

    fix bug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,14 +6,14 @@
 // import './src/application.scss'
 // import './src/example'
 
-import "bootstrap";
+//import "bootstrap";
 
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 
-require("packs/src/application.js");
+//require("packs/src/application.js");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
