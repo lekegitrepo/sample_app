440abe82da210aa7ece280358f36dff41815890b
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-10-30 11:44:28 -0600

    current user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,11 @@ class SessionsController < ApplicationController
     # if user is true/valid and authentication of password is true.
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
     else
       # Create an error message.
-      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
