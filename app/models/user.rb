48d431b196d3754638899ffa9ca6b728b10e0918
lekegitrepo <lekedevrepo@gmail.com>
2019-11-19 14:11:28 -0600

    update authenticate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,10 +42,18 @@ class User < ApplicationRecord
   end
 
   # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
+  # def authenticated?(remember_token)
+  #   return false if remember_digest.nil?
 
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  #   BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  # end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # Forgets a user.
