c74d4ff64bd9f62eae87a1d09a7df0625e4c2701
lekegitrepo <lekedevrepo@gmail.com>
2019-11-20 13:56:15 -0600

    set reset to nil after successful login

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -32,6 +32,7 @@ class PasswordResetsController < ApplicationController
       render 'edit'
     elsif @user.update_attributes(user_params) # Case (4)
       log_in @user
+      @user.update_attribute(:reset_digest, nil) # exercise code
       flash[:success] = 'Password has been reset.'
       redirect_to @user
     else
