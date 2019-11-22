903ab3bcccf221d916c5a3603279c491760f003f
lekegitrepo <lekedevrepo@gmail.com>
2019-11-22 03:04:13 -0600

    add an image upload test template

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -5,7 +5,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "micropost interface" do
+  test 'micropost interface' do
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
@@ -46,5 +46,36 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   #   other_user.microposts.create!(content: 'A micropost')
   #   get root_path
   #   assert_match FILL_IN, response.body
+  # end
+
+  # add an image upload test template
+  # test "micropost interface" do
+  #   log_in_as(@user)
+  #   get root_path
+  #   assert_select 'div.pagination'
+  #   assert_select 'input[type=FILL_IN]'
+  #   # Invalid submission
+  #   post microposts_path, params: { micropost: { content: "" } }
+  #   assert_select 'div#error_explanation'
+  #   # Valid submission
+  #   content = "This micropost really ties the room together"
+  #   picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
+  #   assert_difference 'Micropost.count', 1 do
+  #     post microposts_path, params: { micropost:
+  #                                     { content: content,
+  #                                       picture: FILL_IN } }
+  #   end
+  #   assert FILL_IN.picture?
+  #   follow_redirect!
+  #   assert_match content, response.body
+  #   # Delete a post.
+  #   assert_select 'a', 'delete'
+  #   first_micropost = @user.microposts.paginate(page: 1).first
+  #   assert_difference 'Micropost.count', -1 do
+  #     delete micropost_path(first_micropost)
+  #   end
+  #   # Visit a different user.
+  #   get user_path(users(:archer))
+  #   assert_select 'a', { text: 'delete', count: 0 }
   # end
 end
